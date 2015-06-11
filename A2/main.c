9d1d5c9d64cbfea88ca44282be6f32deae4d5237
Simarpreet Singh <simar@linux.com>
2015-06-10 20:25:06 -0400

    works with debouncing
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- A2/main.c
+++ A2/main.c
@@ -2,41 +2,28 @@
 #include "glcd.h"
 #include <stdio.h>
 
-uint32_t g_ms = 0;
-uint32_t ms_coeff = 1000;
-uint32_t sec_in_min = 60;
-uint32_t ms_in_min = 60000;
-uint32_t min_to_run = 10;
-uint32_t magic_number = 16550;
-uint32_t i = 0;
-uint32_t j = 1;
-uint32_t milliseconds = 100;
-
 char curr_state[16];
 volatile int input;
 volatile uint32_t global_time;
 volatile char counter[64];
-int TS = 3000000;
-int timeSpike = 10000;
-volatile int persist_dot = 1;
-volatile int persist_dash = 1;
-volatile int prev_button_val = 0;
-volatile int global_button_input = 0;
-unsigned char Key_Hit;
-unsigned char Long_Key_Hit;
+int int0_val;
+volatile int tim0_val;
 
-#define MAX_PERSIST 3
+volatile int global_button_input = 0;
+unsigned char Dot_probability;
+unsigned char Dash_probability;
+
 #define TOTAL_STATES 8
 #define TOTAL_EVENTS 2
-//#define BUTTON ~(LPC_GPIO2->FIOPIN >> 10) & 0x01
-volatile unsigned char Div18Counter = 0;
+#define NOISE_HANDLER 3
+#define TS 9999999
 
 typedef enum { S0, S1, S2, S3, S4, S5, S6, S7 } state_t;
 typedef enum { DOT, DASH } event_t;
 	
 state_t transition_matrix[TOTAL_STATES][TOTAL_EVENTS] = {
 	{S1, S0},	// S0
-	{S0, S2}, // S1
+	{S1, S2}, // S1
 	{S1, S3}, // S2
 	{S4, S0}, // S3
 	{S1, S5}, // S4
@@ -61,73 +48,68 @@ void init_intr()
 	
 	LPC_GPIOINT->IO2IntEnF |= 1 << 10; // falling edge of P2.10		BUTTON IS PRESSED
 	LPC_GPIOINT->IO2IntEnR |= 1 << 10; // rising edge of P2.10	BUTTON IS RELEASED
-	//NVIC_EnableIRQ(EINT3_IRQn);
+	NVIC_EnableIRQ(EINT3_IRQn);
 	
+	// Timer to handle debouncing
 	LPC_TIM1->TCR = 0x02;	// reset timer
 	LPC_TIM1->TCR = 0x01; // enable timer
-	LPC_TIM1->MR0 = 4096; // match register TODO: Check what value works.
+	LPC_TIM1->MR0 = 1024; // match register
 	LPC_TIM1->MCR |= 0x03; // on match, generate interrupt and reset
 	NVIC_EnableIRQ(TIMER1_IRQn); // enable timer interrupts
 	
 }
 
 void TIMER1_IRQHandler() {
-		LPC_TIM1->IR |= 0x01;	// clear the interrupt.
-
-		global_button_input =~ (LPC_GPIO2->FIOPIN >> 10) & 0x01;
+		static unsigned char noise_counter = 0;
+		int button_input =~ (LPC_GPIO2->FIOPIN >> 10) & 0x01;
 	
-		if(global_button_input == 0) {
-				if(Key_Hit != 0) {
-					Key_Hit--;
+		LPC_TIM1->IR |= 0x01;	// clear the interrupt.
+	
+		if(button_input == 1) {
+				if(Dot_probability != 0) {
+					Dot_probability--;
 				}
 		}
 		else {
-			if(Key_Hit != 0xff) {
-					Key_Hit++;
+			if(Dot_probability != 0xff) {
+					Dot_probability++;
 			}
 		}
 
-		if(++Div18Counter >= 5) {
-			Div18Counter = 0;
-			if(Key_Hit == 0xff) {
-				if(Long_Key_Hit != 0xff)
-					Long_Key_Hit++;
+		if(++noise_counter >= NOISE_HANDLER) {
+			noise_counter = 0;
+			if(Dot_probability == 0xff) {
+				if(Dash_probability != 0xff)
+					Dash_probability++;
 			}
-			else if(Key_Hit == 0x00)
-				Long_Key_Hit = 0x00;
-		}
+			else if(Dot_probability == 0x00)
+				Dash_probability = 0x00;
+		}	
 }
 
 
 void EINT3_IRQHandler()
 {
-	int int0_val;
-	volatile int tim0_val;
-	
 	// Clear the interrupt on EINT0
 	LPC_GPIOINT->IO2IntClr |= 1 << 10;
 	
 	int0_val =~ (LPC_GPIO2->FIOPIN >> 10) & 0x01;
-	//tim0_val = LPC_TIM0->TC;
 
 	if (int0_val == 1)		// Button is pressed
 	{
 		// Start Timer
-	  LPC_TIM0->TCR = 0x02;
+		global_time = 0;
 		LPC_TIM0->TCR = 0x01;
-		//LPC_TIM0->MR0 = 25000;
 		LPC_TIM0->PR  = 0x00; 
 	}
 	else if (int0_val == 0)		// Button is released
 	{
-		tim0_val = LPC_TIM0->TC;
 		// Stop Timer
+		tim0_val = LPC_TIM0->TC;
+		global_time = tim0_val;
 		LPC_TIM0->TCR = 0x02;
+		LPC_TIM0->PR  = 0x00; 
 	}
-
-	global_time = tim0_val;
-	//global_button_input = int0_val;
-	
 }
 
 int main (void)
@@ -139,36 +121,39 @@ int main (void)
 	init_system();
 	init_intr();
 	
+	GLCD_DisplayString(3,1,1,"INPUT:    ");
+	GLCD_DisplayString(1, 1, 1, "STATE: ");
+	sprintf(curr_state, "%02d", current_state);
+	GLCD_DisplayString(1, 8, 1, (unsigned char *)curr_state);
+	
 	while (!pattern_match)
 	{
-		GLCD_DisplayString(7,1,1,"STATUS: RUNNING");
-
-		//checkIfDotOrDash();
+			GLCD_DisplayString(7,1,1,"STATUS: RUNNING");
 		
-			if (Key_Hit == 0xff) {
+			if (Dot_probability == 0xff && global_time < TS && global_time > 0) {
 				current_event = DOT;
 				GLCD_DisplayString(3,1,1,"INPUT:  DOT");
-				sprintf(curr_state, "%02d", current_state);
+	
 				current_state = transition_matrix[current_state][current_event];
-				GLCD_DisplayString(1, 1, 1, (unsigned char *)curr_state);
+				sprintf(curr_state, "%02d", current_state);
+				GLCD_DisplayString(1, 8, 1, (unsigned char *)curr_state);
+				
+				global_time = 0;
 			}
-			else if (Key_Hit == 0x00) {
-				// Release
-			}
-			else if (Long_Key_Hit == 0xff) {
+			else if (Dash_probability == 0xff && global_time >= TS && global_time > 0) {
 				current_event = DASH;
 				GLCD_DisplayString(3,1,1,"INPUT: DASH");
+
 				current_state = transition_matrix[current_state][current_event];
 				sprintf(curr_state, "%02d", current_state);
-				GLCD_DisplayString(1, 1, 1, (unsigned char *)curr_state);
+				GLCD_DisplayString(1, 8, 1, (unsigned char *)curr_state);
+				
+				global_time = 0;
 			}
-		
-			
 			
 			if (current_state == S7) { 
 				pattern_match = 1;
-				GLCD_Clear(White);
-				GLCD_DisplayString(7,1,1,"STATUS: COMPLETE");
+				GLCD_DisplayString(7,1,1,"STATUS: CORRECT!");
 			}
 	}
 }
