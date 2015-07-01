7f6d21a717b87e55bb680e06e288936fdbded619
Simarpreet Singh <simar@linux.com>
2015-07-01 19:41:28 -0400

    fixing a interrupt clear bug
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- A3/main.c
+++ A3/main.c
@@ -16,6 +16,7 @@ char curr_count[16];
 volatile int input;
 volatile uint32_t global_time;
 volatile char counter[64];
+char cur_time[128];
 int int0_val;
 volatile int tim0_val;
 
@@ -41,12 +42,19 @@ void init_intr()
 	//LPC_GPIOINT->IO2IntEnR |= 1 << 10; // rising edge of P2.10	BUTTON IS RELEASED
 	NVIC_EnableIRQ(EINT3_IRQn);
 	
-	// Timer to handle debouncing
+	// Interrupt scheduler timer
 	LPC_TIM1->TCR = 0x02;	// reset timer
 	LPC_TIM1->TCR = 0x01; // enable timer
 	LPC_TIM1->MR0 = FIVE_SEC; // match register
 	LPC_TIM1->MCR |= 0x03; // on match, generate interrupt and reset
 	NVIC_EnableIRQ(TIMER1_IRQn); // enable timer interrupts
+	
+	// Timer to see time on the screen
+	LPC_TIM0->TCR = 0x02;	// reset timer
+	LPC_TIM0->TCR = 0x01; // enable timer
+	LPC_TIM0->MR0 = 25000; // match value (10 minute mark)
+	LPC_TIM0->MCR |= 0x03; // on match, generate interrupt and reset
+	NVIC_EnableIRQ(TIMER0_IRQn); // enable timer interrupts
 	
 	// LED init
 	LPC_GPIO1->FIODIR |= 0xB0000000;	// LEDs on Port 1	
@@ -54,8 +62,15 @@ void init_intr()
 	
 }
 
+void TIMER0_IRQHandler() {
+		LPC_TIM0->IR |= 0x01;	// clear the interrupt.
+		//GLCD_DisplayString(9, 1, 1, "Interrupt hit")
+		g_ms++;
+}
+
 void TIMER1_IRQHandler() {
 		LPC_TIM1->IR |= 0x01;	// clear the interrupt.
+		LPC_GPIOINT->IO2IntClr |= 1 << 10;		// Clear the EINT3 interrupt.
 		NVIC_ClearPendingIRQ(EINT3_IRQn);
 		NVIC_EnableIRQ(EINT3_IRQn);		// Re-enable the button interrupt.
 }
@@ -65,13 +80,14 @@ void EINT3_IRQHandler()
 {
 	// Clear the interrupt on EINT0
 	LPC_GPIOINT->IO2IntClr |= 1 << 10;
-		
+
 	// Turn on the LED
 	LPC_GPIO1->FIOSET = 1 << LED_PIN;
 
 	button_press_count = button_press_count + 1;
 	
-	int0_val =~ (LPC_GPIO2->FIOPIN >> 10) & 0x01;
+	// Turn off the LED
+	LPC_GPIO1->FIOSET = 0 << LED_PIN;
 	
 	NVIC_DisableIRQ(EINT3_IRQn);
 }
@@ -91,8 +107,9 @@ int main (void)
 			GLCD_DisplayString(7,1,1,"STATUS: RUNNING");
 			sprintf(curr_count, "%02d", button_press_count);
 			GLCD_DisplayString(1, 8, 1, (unsigned char *)curr_count);
-		
-			for(led_delay = 0; led_delay < 100; ++led_delay) {}
+					
+			sprintf(cur_time, "%02d:%02d", ((g_ms)/ms_in_min),((g_ms)/ms_coeff)%sec_in_min);
+			GLCD_DisplayString(5, 5, 1, (unsigned char *)cur_time);
 			
 			// Turn off the LED
 			LPC_GPIO1->FIOSET = 0 << 28;
